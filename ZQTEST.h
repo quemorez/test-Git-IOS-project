5230a47ff79c6878e566cce533462122f3a809d6
Alyssa DiMare <alyssadimare@Alyssas-MacBook-Pro.local>
2014-05-06 17:15:01 -0700

    added test class

---
+++ ZQTEST.h
@@ -0,0 +1,13 @@
+//
+//  ZQTEST.h
+//  Git Test
+//
+//  Created by Alyssa DiMare on 5/6/14.
+//  Copyright (c) 2014 ZQ production. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZQTEST : NSObject
+
+@end
---
+++ ZQTEST.m
@@ -0,0 +1,13 @@
+//
+//  ZQTEST.m
+//  Git Test
+//
+//  Created by Alyssa DiMare on 5/6/14.
+//  Copyright (c) 2014 ZQ production. All rights reserved.
+//
+
+#import "ZQTEST.h"
+
+@implementation ZQTEST
+
+@end
